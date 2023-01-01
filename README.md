dfdd1c4854826cc4dca10dc5a5fb77aff78d3ea2
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2023-01-01 16:49:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:55c7998ef40e4cb796b070dc9a25139f3cecba7d3e4e28570cce51268b082f40
+size 375
---
+++ script.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d554ddf5712449f5d5271a01ec5a4b6cb349375946e067b9a275fc5ecaabb0f9
+size 238
